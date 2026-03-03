1940a3faea6c1d7bcb780f4aee071f95a6888f68
All-in-one Mighty <AllInOneMighty@users.noreply.github.com>
2026-03-03 10:52:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SharedMediaAdditionalSounds
+
+Inserts more than **15 additional sounds** into LibSharedMedia's storage.
+
+LibSharedMedia is used in an estimated **~1,250+ addons** and downloaded over
+**100 million times**.
+
+LibSharedMedia is used as a library in _other_ addons to add resources such as
+fonts, sounds, images, … By installing SharedMediaAdditionalSounds, you add
+sounds to the list of sounds provided by LibSharedMedia to these addons. If your
+other addons do not use LibSharedMedia, this addon (SharedMediaAdditionalSounds)
+doesn't do anything.
---
+++ SharedMediaAdditionalSounds.lua
@@ -0,0 +1,42 @@
+--[[
+
+----
+---- PLEASE READ BEFORE DOING ANYTHING ----
+----
+
+Only sounds should be added to this addon. If you wish to add textures, fonts, ...
+create your own addon or use an existing one if there is one.
+
+Don't add a sound that you did not test. All sounds must be tested in game before upload.
+
+Thanks for your cooperation! ;-)
+All-in-one Mighty
+
+If you edit this file, be careful not to overwrite it when you update this addon.
+
+]]
+
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+local LSM = LibStub("LibSharedMedia-3.0", true)
+
+-- -----
+-- SOUND
+-- -----
+LSM:Register("sound", "Bell Toll Alliance", 566564)
+LSM:Register("sound", "Bell Toll Horde", 565853)
+LSM:Register("sound", "Auction Window Close", 567499)
+LSM:Register("sound", "Quest Failed", 567459)
+LSM:Register("sound", "Fel Nova", 568582)
+LSM:Register("sound", "Bad Press", 568975)
+LSM:Register("sound", "Simon Large Blue", 566076)
+LSM:Register("sound", "Simon Small Blue", 567002)
+LSM:Register("sound", "Portcullis Close", 566240)
+LSM:Register("sound", "Run Away", 552035)
+LSM:Register("sound", "PvP Flag Taken", 569200)
+LSM:Register("sound", "Arcane Crystal", 565425)
+LSM:Register("sound", "Bellow Out", 565530)
+LSM:Register("sound", "Cannon", 566101)
+LSM:Register("sound", "Firework 3", 566862)
+LSM:Register("sound", "Alarm Clock Warning 2", 567399)
+
